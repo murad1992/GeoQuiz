dc48cbedd9d24b7953911c3edfa9d7e7457c78e1
Murad Muradov <murkagam@mail.ru>
2016-01-02 21:32:39 +0300

    Добавил TrueFalse

--- app/src/main/java/com/example/murad/geoquiz/TrueFalse.java
+++ app/src/main/java/com/example/murad/geoquiz/TrueFalse.java
@@ -1,12 +1,21 @@
 package com.example.murad.geoquiz;
 
 public class TrueFalse {
-    public int mQuestion;
-    public boolean mTrueQuestion;
 
-    public TrueFalse(int question, boolean trueQuestion){
+    public int mQuestion;
+    public int getQuestion() {return mQuestion;}
+    public void setQuestion(int question) {mQuestion = question;}
+
+
+    public boolean mTrueQuestion;
+    public boolean isTrueQuestion() {return mTrueQuestion;}
+    public void setTrueQuestion(boolean trueQuestion) {mTrueQuestion = trueQuestion;}
+
+
+
+
+    public TrueFalse(int question, boolean trueQuestion) {
         mQuestion = question;
-        mTrueQuestion = trueQuestion;
-    }
+        mTrueQuestion = trueQuestion;}
 
 }
