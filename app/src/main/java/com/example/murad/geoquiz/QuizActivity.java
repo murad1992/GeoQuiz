43314f3cf99d02713b0a35b80c6c454d541c9059
Murad Muradov <murkagam@mail.ru>
2016-01-08 19:44:27 +0300

    Перещел на новую ветку для тестовых заданий, теперь TextView тоже умеет менять вопросы

--- app/src/main/java/com/example/murad/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/murad/geoquiz/QuizActivity.java
@@ -81,6 +81,13 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+        public void onClick(View v){
+                updateQuestion();
+            }
+        });
+
         updateQuestion();
         /////////////////////////////////////////////////////////////////////////////////////
     }
