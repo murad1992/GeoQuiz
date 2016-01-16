5b9febc6d5cf1e7d201d38c992c18852f3990c88
Murad Muradov <murkagam@mail.ru>
2016-01-17 00:37:52 +0300

    Добавил альбомную ориентацию и папку с layout , называется layout-land для альбомной ориентации

--- app/src/main/java/com/example/murad/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/murad/geoquiz/QuizActivity.java
@@ -47,7 +47,7 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         ////////////////////////////////////////////////////////////////////////////////////
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"OnCreate(Bundle) вызвалось блеять");
+        Log.d(TAG,"onCreate(Bundle)----------------вызвалось блеять");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -87,6 +87,35 @@ public class QuizActivity extends AppCompatActivity {
 
         updateQuestion();
         /////////////////////////////////////////////////////////////////////////////////////
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart()---------------------called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause()---------------------called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume()---------------------called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop()---------------------called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy()--------------------called");
     }
 
     @Override
